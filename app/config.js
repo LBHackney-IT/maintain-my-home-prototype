717be0c48a0d0c32d6d0b782c60c1e3c4e5cdb34
Robert Lee-Cann <leeky@leeky.org.uk>
2017-09-05 16:18:42 +0100

    (chore) Copy tweaks

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Report a repair',
+  serviceName: 'Maintain my home',
 
   // Default port that prototype runs on
   port: '3000',
